ee3a208c0d8a91e57cbb870a1933ac90c1f3e454
andrey31453 <andrey31453@google.com>
2024-03-04 16:45:15 +0300

    ghjkghdfkj hgjdhg kjdfhg kjdgh kjdgfh dk

--- webpack.config.from_manifest.js
+++ webpack.config.from_manifest.js
@@ -9,18 +9,30 @@ module.exports = ({ dev }) => ({
       name: 'host',
       filename: 'remote_entry.js',
 			exposes: {
-				'./t1': src + '/a1', 
-				'./t2': src + '/a2', 
-				'./t3': src + '/a3', 
-			},
+'./t1': src + '/a1',
+'./t2': src + '/a2',
+'./t3': src + '/a3'
+}, 
 			remotes: {
-				'./r1': `${dev ? 'http://localhost' : 'https://prod'}:8003/remote_entry.js`, 
-			},
-      shared: {
-				'react': { singleton: true },
-        'react-dom': { singleton: true },
-				
-      },
+'./r1': `${dev ? 'http://localhost' : 'https://prod'}:8003/remote_entry.js`
+}, 
+			shared: {
+'react': { singleton: true },
+'react-dom': { singleton: true }
+}, 
     }),
 	],
+
+	resolve: {
+		alias: {
+'~types': src + './src/types',
+'~config': src + './src/config',
+'~decorators': src + './src/decorators',
+'~file_data_creators': src + './src/file_data_creators',
+'~bd': src + './src/bd',
+'~modules': src + './src/modules',
+'~utils': src + './src/utils',
+'~fs': src + './src/fs'
+}, 
+	},
 })
