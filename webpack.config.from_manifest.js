f683e564538625781348983abf437535fa076261
andrey31453 <andrey31453@google.com>
2024-03-05 15:31:38 +0300

    f

--- webpack.config.from_manifest.js
+++ webpack.config.from_manifest.js
@@ -6,33 +6,33 @@ const src = path.join(__dirname, './src')
 module.exports = ({ dev }) => ({
 	plugins: [
 		new module_federation({
-      name: 'host',
-      filename: 'remote_entry.js',
+			name: 'host',
+			filename: 'remote_entry.js',
 			exposes: {
-'./t1': src + '/a1',
-'./t2': src + '/a2',
-'./t3': src + '/a3'
-}, 
+				'./t1': src + '/a1',
+				'./t2': src + '/a2',
+				'./t3': src + '/a3',
+			},
 			remotes: {
-'./r1': `${dev ? 'http://localhost' : 'https://prod'}:8003/remote_entry.js`
-}, 
+				'./r1': `${dev ? 'http://localhost' : 'https://prod'}:8003/remote_entry.js`,
+			},
 			shared: {
-'react': { singleton: true },
-'react-dom': { singleton: true }
-}, 
-    }),
+				'react': { singleton: true },
+				'react-dom': { singleton: true },
+			},
+		}),
 	],
 
 	resolve: {
 		alias: {
-'~types': src + './src/types',
-'~config': src + './src/config',
-'~decorators': src + './src/decorators',
-'~file_data_creators': src + './src/file_data_creators',
-'~bd': src + './src/bd',
-'~modules': src + './src/modules',
-'~utils': src + './src/utils',
-'~fs': src + './src/fs'
-}, 
+			'~types': src + './src/types',
+			'~config': src + './src/config',
+			'~decorators': src + './src/decorators',
+			'~file_data_creators': src + './src/file_data_creators',
+			'~bd': src + './src/bd',
+			'~modules': src + './src/modules',
+			'~utils': src + './src/utils',
+			'~fs': src + './src/fs',
+		},
 	},
 })
--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,4 @@ const generate = require('./webpack.config.from_manifest')
 const local = require('./webpack.config.local')
 
 module.exports = (props) =>
-  merge(template(props), generate(props), local(props))
+	merge(template(props), generate(props), local(props))
