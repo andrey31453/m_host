7cea58ac917349cf3ec39b996249f40ab09fbcd7
andrey31453 <andrey31453@google.com>
2024-03-11 14:47:34 +0300

    fff

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+// @ts-ignore
+import App1_Header from 'app1/Header'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Host</h1>
+      </div>
+
+      <hr />
+
+      <App1_Header />
+    </>
+  )
+}
+
+export default App
