4c98529d70a5be610010a90c99991ba45f81bc0a
andrey31453 <andrey31453@google.com>
2024-03-14 18:04:11 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
 import { Test } from '~widgets'
+import { test1 } from 'arora-npm-ts'
+import './test.scss'
 
 // @ts-ignore
 const App1_Header = React.lazy(() => import('app1/Header'))
 
 const App = () => {
+  test1()
+
   return (
     <>
-      <h1>Host</h1>
+      <h1 className='test'>Host</h1>
       <hr />
       <Test />
       <hr />
